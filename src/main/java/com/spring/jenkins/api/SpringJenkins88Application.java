20982fd55f24ec264e1179306de5c1ef1215fb85
zulfikar4ansari <zulfikar4ansari@gmail>
2023-06-19 16:01:37 +0530

    First Commit

---
+++ src/main/java/com/spring/jenkins/api/SpringJenkins88Application.java
@@ -0,0 +1,13 @@
+package com.spring.jenkins.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkins88Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkins88Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jenkins/api/SpringJenkins88ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jenkins.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkins88ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
